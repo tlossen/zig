0c89d6e27980594cfacad1f69caeb7393175f1a4
Tim Lossen <tim@lossen.de>
2012-11-05 23:23:42 +0100

    added format command

--- lib/jmie.rb
+++ lib/jmie.rb
@@ -1,6 +1,7 @@
 require "jmie/version"
 require "jmie/lines"
 require "jmie/parser"
+require "jmie/formatter"
 
 require "stringio"
 
@@ -11,6 +12,10 @@ module JMIE
     start = lines.current || raise("cannot parse empty document")
     lines.next
     Parser.parse_value(0, lines, start)
+  end
+
+  def self.print(data)
+    Formatter.print_value("", data)
   end
 
 end
---
+++ lib/jmie/formatter.rb
@@ -0,0 +1,45 @@
+class JMIE::Formatter
+
+  INDENT = "   "
+
+  def self.print_hash(indent, hash)
+    print "{\n"
+    indent = indent + INDENT
+    hash.each do |key, value|
+      print indent + key.to_s + ": "
+      print_value(indent, value)
+    end
+  end
+
+  def self.print_array(indent, array)
+    print "[\n"
+    indent = indent + INDENT
+    array.each do |v|
+      print indent
+      print_value(indent, v)
+    end
+  end
+
+  def self.print_multiline_string(indent, string)
+    print "\"\n"
+    indent = indent + INDENT
+    string.split("\n").each do |line|
+      print indent + line.lstrip + "\n"
+    end
+  end
+
+  def self.print_value(indent, value)
+    case value
+    when Hash then print_hash(indent, value)
+    when Array then print_array(indent, value)
+    when String
+      if value.include?("\n")
+        print_multiline_string(indent, value)
+      else
+        print "'#{value}'\n"
+      end
+    else print value.inspect + "\n"
+    end
+  end
+
+end
